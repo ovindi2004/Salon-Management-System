8e0cba2ad79bc2422efb8cdf02108168f2319e6a
ovindi Jayawardena <ovindijayawardena@gmail.com>
2026-08-25 11:51:15 +0530

    creat  dto entityv ChangePasswordDTO

--- Salon-Management-System/src/main/java/com/example/Salon_Management_System/dto/ChangePasswordDTO.java
+++ Salon-Management-System/src/main/java/com/example/Salon_Management_System/dto/ChangePasswordDTO.java
@@ -1,4 +1,15 @@
 package com.example.Salon_Management_System.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ChangePasswordDTO {
+
+    private Long userId;
+    private String currentPassword;
+    private String newPassword;
 }
