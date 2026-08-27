1b9428fd13eb8b953a9a24e5a751bbcb9d32d069
ovindi Jayawardena <ovindijayawardena@gmail.com>
2026-08-27 21:14:40 +0530

    creat new StaffAvailability enum

--- Salon-Management-System/src/main/java/com/example/Salon_Management_System/entity/Staff.java
+++ Salon-Management-System/src/main/java/com/example/Salon_Management_System/entity/Staff.java
@@ -1,4 +1,24 @@
 package com.example.Salon_Management_System.entity;
 
+import com.example.Salon_Management_System.enumiration.UserStatus;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+
+import java.time.LocalDate;
+
 public class Staff {
+    private Long staffId;
+    private String staffName;
+    private String staffEmail;
+    private String staffPhone;
+    private LocalDate dateOfBirth;
+    private String gender;
+    private  String address;
+    private LocalDate hireDate;
+    private Double salary;
+
+    @Enumerated(EnumType.STRING)
+    private UserStatus status;
+
+
 }
--- Salon-Management-System/src/main/java/com/example/Salon_Management_System/enumiration/StaffAvailability.java
+++ Salon-Management-System/src/main/java/com/example/Salon_Management_System/enumiration/StaffAvailability.java
@@ -1,6 +1,4 @@
 package com.example.Salon_Management_System.enumiration;
 
-public enum Availability {
-    Available,
-    Unavailable
+public enum StaffAvailability {
 }
