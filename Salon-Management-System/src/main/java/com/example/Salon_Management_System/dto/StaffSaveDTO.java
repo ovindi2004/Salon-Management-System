dd6e9a3903b489644b1f0d71025e6641e393afa3
ovindi Jayawardena <ovindijayawardena@gmail.com>
2026-08-27 21:36:37 +0530

    complet staffSaveDTO

--- Salon-Management-System/src/main/java/com/example/Salon_Management_System/dto/StaffSaveDTO.java
+++ Salon-Management-System/src/main/java/com/example/Salon_Management_System/dto/StaffSaveDTO.java
@@ -1,4 +1,22 @@
 package com.example.Salon_Management_System.dto;
 
+import com.example.Salon_Management_System.enumiration.UserStatus;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public class StaffSaveDTO {
+    private String staffName;
+    private String staffEmail;
+    private String staffPhone;
+    private LocalDate dateOfBirth;
+    private String gender;
+    private String address;
+    private String position;
+    private LocalDate hireDate;
+    private Double salary;
+    private UserStatus status;
+    private String username;
+    private String password;
+    private List<Long> serviceIds;
 }
