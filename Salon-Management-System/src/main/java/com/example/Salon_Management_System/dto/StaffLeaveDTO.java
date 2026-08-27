7345e5cbe878d7ed273e6e275934a57cd3ae1216
ovindi Jayawardena <ovindijayawardena@gmail.com>
2026-08-27 21:52:59 +0530

    complet StaffLeaveDTO

--- Salon-Management-System/src/main/java/com/example/Salon_Management_System/dto/StaffLeaveDTO.java
+++ Salon-Management-System/src/main/java/com/example/Salon_Management_System/dto/StaffLeaveDTO.java
@@ -1,4 +1,21 @@
 package com.example.Salon_Management_System.dto;
 
+import com.example.Salon_Management_System.enumiration.LeaveStatus;
+import com.example.Salon_Management_System.enumiration.LeaveType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class StaffLeaveDTO {
+    private Long leaveId;
+    private  Long staffId;
+    private LeaveType leaveType;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String reason;
+    private LeaveStatus status;
 }
