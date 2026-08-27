d94007b3ffeae9966520a1e76658996eb72e09b7
ovindi Jayawardena <ovindijayawardena@gmail.com>
2026-08-27 22:05:57 +0530

    add anotation StaffServiceImpl

--- Salon-Management-System/src/main/java/com/example/Salon_Management_System/service/impl/StaffServiceImpl.java
+++ Salon-Management-System/src/main/java/com/example/Salon_Management_System/service/impl/StaffServiceImpl.java
@@ -1,4 +1,11 @@
 package com.example.Salon_Management_System.service.impl;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class StaffServiceImpl {
 }
