ebde78462011652d11add43c4cc7405a536a1022
ovindi Jayawardena <ovindijayawardena@gmail.com>
2026-08-27 21:50:57 +0530

    complet StaffWorkingHourDTO

--- Salon-Management-System/src/main/java/com/example/Salon_Management_System/dto/StaffWorkingHourDTO.java
+++ Salon-Management-System/src/main/java/com/example/Salon_Management_System/dto/StaffWorkingHourDTO.java
@@ -1,4 +1,17 @@
 package com.example.Salon_Management_System.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class StaffWorkingHourDTO {
+    private Long workingHourId;
+    private String day;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private boolean dayOff;
 }
