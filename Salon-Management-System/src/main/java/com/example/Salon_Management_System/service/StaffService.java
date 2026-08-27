8a7f8dbee0fb472cd0643a5eb66b207d303158ec
ovindi Jayawardena <ovindijayawardena@gmail.com>
2026-08-27 22:03:35 +0530

    cerate all method StaffService

--- Salon-Management-System/src/main/java/com/example/Salon_Management_System/service/StaffService.java
+++ Salon-Management-System/src/main/java/com/example/Salon_Management_System/service/StaffService.java
@@ -1,4 +1,22 @@
 package com.example.Salon_Management_System.service;
 
+import com.example.Salon_Management_System.dto.StaffDTO;
+import com.example.Salon_Management_System.dto.StaffLeaveDTO;
+import com.example.Salon_Management_System.dto.StaffWorkingHourDTO;
+
+import java.util.List;
+
 public interface StaffService {
+    StaffDTO createStaff(StaffDTO staffDTO);
+    StaffDTO getStaffById(Long staffId);
+    List<StaffDTO> getAllStaff();
+    List<StaffDTO>searchStaff(String keyword);
+    StaffDTO updateStaff(Long staffId, StaffDTO staffDTO);
+    void deleteStaff(Long staffId);
+    void updateStatus(Long staffId);
+    void updateWorkingHours(Long staffId, List<StaffWorkingHourDTO> dto);
+    List<StaffWorkingHourDTO> getWorkingHours(Long staffId);
+    StaffLeaveDTO addLeave(Long staffId, StaffLeaveDTO dto);
+    List<StaffLeaveDTO> getLeaves(Long staffId);
+    void updateLeave(Long leaveId, String status);
 }
