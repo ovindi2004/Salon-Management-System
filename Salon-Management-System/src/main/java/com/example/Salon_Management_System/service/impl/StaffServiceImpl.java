3bd1f06d222dac10cd816440c505e2018e5df8e2
ovindi Jayawardena <ovindijayawardena@gmail.com>
2026-08-27 22:07:22 +0530

    implement StaffServiceImpl in StaffService

--- Salon-Management-System/src/main/java/com/example/Salon_Management_System/service/impl/StaffServiceImpl.java
+++ Salon-Management-System/src/main/java/com/example/Salon_Management_System/service/impl/StaffServiceImpl.java
@@ -1,11 +1,76 @@
 package com.example.Salon_Management_System.service.impl;
 
+import com.example.Salon_Management_System.dto.StaffDTO;
+import com.example.Salon_Management_System.dto.StaffLeaveDTO;
+import com.example.Salon_Management_System.dto.StaffWorkingHourDTO;
+import com.example.Salon_Management_System.service.StaffService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class StaffServiceImpl {
+public class StaffServiceImpl implements StaffService {
+    @Override
+    public StaffDTO createStaff(StaffDTO staffDTO) {
+        return null;
+    }
+
+    @Override
+    public StaffDTO getStaffById(Long staffId) {
+        return null;
+    }
+
+    @Override
+    public List<StaffDTO> getAllStaff() {
+        return List.of();
+    }
+
+    @Override
+    public List<StaffDTO> searchStaff(String keyword) {
+        return List.of();
+    }
+
+    @Override
+    public StaffDTO updateStaff(Long staffId, StaffDTO staffDTO) {
+        return null;
+    }
+
+    @Override
+    public void deleteStaff(Long staffId) {
+
+    }
+
+    @Override
+    public void updateStatus(Long staffId) {
+
+    }
+
+    @Override
+    public void updateWorkingHours(Long staffId, List<StaffWorkingHourDTO> dto) {
+
+    }
+
+    @Override
+    public List<StaffWorkingHourDTO> getWorkingHours(Long staffId) {
+        return List.of();
+    }
+
+    @Override
+    public StaffLeaveDTO addLeave(Long staffId, StaffLeaveDTO dto) {
+        return null;
+    }
+
+    @Override
+    public List<StaffLeaveDTO> getLeaves(Long staffId) {
+        return List.of();
+    }
+
+    @Override
+    public void updateLeave(Long leaveId, String status) {
+
+    }
 }
