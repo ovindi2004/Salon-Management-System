321d3b46a31942b91c02cb8f194930aa780ef93f
ovindi Jayawardena <ovindijayawardena@gmail.com>
2026-08-27 19:59:15 +0530

    creat new StaffController

---
+++ Salon-Management-System/src/main/java/com/example/Salon_Management_System/controller/StaffController.java
@@ -0,0 +1,4 @@
+package com.example.Salon_Management_System.controller;
+
+public class StaffController {
+}
--- Salon-Management-System/src/main/java/com/example/Salon_Management_System/security/StaffService.java
+++
@@ -1,4 +0,0 @@
-package com.example.Salon_Management_System.security;
-
-public interface StaffService {
-}
