b3bc303e12f3dbf8de3192d7ec85093a14e1a672
ovindi Jayawardena <ovindijayawardena@gmail.com>
2026-09-06 01:41:54 +0530

    complet service dto

---
+++ Salon-Management-System/src/main/java/com/example/Salon_Management_System/dto/ServiceDTO.java
@@ -0,0 +1,21 @@
+package com.example.Salon_Management_System.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ServiceDTO {
+    private Long serviceId;
+    private String serviceName;
+    private String category;
+    private String description;
+    private BigDecimal price;
+    private Integer duration;
+    private String status;
+    private List<Long> staffIds;.
+}
